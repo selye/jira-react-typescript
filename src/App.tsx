d8c217ef61aa304b80cda49590af72f86255dec1
shijie <1193033232@qq.com>
2021-12-05 01:22:24 +0800

    fix: 添加文件配置功能

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-
-
         </p>
-        <a
-          className="App-link"
+        <a className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
